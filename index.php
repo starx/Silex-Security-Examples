97e8c967803dafc40ca8350251b2890354123230
starx <swe_r3p0x9@mrnepal.com>
2023-09-08 14:30:05 +0100

    add white listed ip voter

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+
+use Symfony\Component\Security\Core\Authorization\Voter\AuthenticatedVoter;
+use Symfony\Component\Security\Core\Authorization\Voter\RoleHierarchyVoter;
+use Symfony\Component\Security\Core\Role\RoleHierarchy;
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -14,14 +19,13 @@ $app->register(new Silex\Provider\SessionServiceProvider());
 $app->register(new Silex\Provider\SecurityServiceProvider());
 $app->register(new \Silex\Provider\UrlGeneratorServiceProvider());
 
-
 $app['security.encoder.digest'] = function ($app) {
     return new Symfony\Component\Security\Core\Encoder\PlaintextPasswordEncoder();
 };
+
 $app['security.firewalls'] = array(
     'secured' => array(
-        'pattern' => '^/',  // Protects the whole application, change as needed
-        'anonymous' => true,
+        'pattern' => '^.*$',  // Protects the whole application, change as needed
         'form' => array('login_path' => '/login', 'check_path' => '/login_check'),
         'logout' => array('logout_path' => '/logout'),
         'users' => array(
@@ -31,14 +35,31 @@ $app['security.firewalls'] = array(
     ),
 );
 
+$app['security.access_manager'] = function ($app) {
+    return new Symfony\Component\Security\Core\Authorization\AccessDecisionManager(
+        $app['security.voters'],
+        'unanimous',  // This is the strategy
+        false,        // Whether to allow access if all voters abstain. Set to true if you want to allow by default.
+        true          // Whether to allow access if equal grant and deny votes are received.
+    );
+};
+
 $app['security.voters'] = $app->extend('security.voters', function($voters) use ($app) {
-    $voters[] = new \App\RoleVoter();
+//    $voters[] = new RoleHierarchyVoter(new RoleHierarchy($app['security.role_hierarchy']));
+//    $voters[] = new AuthenticatedVoter($app['security.trust_resolver']);
+
+    $whitelistedIps = ['127.0.0.1', '192.16.56.1'];  // Adjust this to your needs
+    $voters[] = new \App\IpVoter($app['request_stack'], $whitelistedIps);
+
+
     return $voters;
 });
 
 $app['security.access_rules'] = array(
-    array('^/admin', 'ROLE_ADMIN'),
-    array('^/user', 'ROLE_USER')
+    array('^/admin', ['IS_AUTHENTICATED_FULLY', 'WHITELISTED_IP', 'ROLE_ADMIN',]),
+    array('^/secured', ['IS_AUTHENTICATED_FULLY', 'WHITELISTED_IP', 'ROLE_USER']),
+    array('^/login', ['IS_AUTHENTICATED_ANONYMOUSLY']),
+    array('^.*$', ['IS_AUTHENTICATED_FULLY', 'WHITELISTED_IP', 'ROLE_USER',]),
 );
 
 $app->get('/', function(\Symfony\Component\HttpFoundation\Request $request) use ($app) {
